db6984477234fc6936bb7d11289d4dbc551d07ff
Aleksander <asava@sibmail.com>
2022-12-24 20:48:45 +0700

    Ожидаемые и неожиданные ошибки

--- src/app/pages/editQuality.jsx
+++ src/app/pages/editQuality.jsx
@@ -1,10 +1,46 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import EditForm from "../components/ui/editForm";
 
+
 const EditQualityPage = () => {
+    const [quality, setQuality] = useState(null)
+    const id = useParams().id
+    const qualityEndPoint = `http://localhost:4000/api/v1/quality/${id}`
+    
+    const handleSubmit = async (data) => {
+        try {
+            await axios
+            .put(qualityEndPoint + 'fg', data)
+            .then((res) => console.log(res.data.content))
+            
+        } catch (error) {
+            const expectedErrors =
+                error.response &&
+                error.response.status >= 400 &&
+                error.response.status < 500
+            if (!expectedErrors) {
+                console.log('Упал сервер или чтото подобное')
+            } else {
+                console.log('Ожидаемая ошибка')
+            }
+            
+        }
+        
+    }
+
+    useEffect(() => {
+        const loadData = async () => {
+            const { data } = await axios.get(qualityEndPoint)
+            setQuality(data.content)
+        }
+         loadData() 
+      }, []) 
     return (
         <>
-            <h1>Edit Quality Page</h1> <EditForm />
+            <h1>Edit Quality Page</h1>{' '}
+            {quality === null ? 'Loading...' : <EditForm data={quality} onSubmit={handleSubmit}/>}
         </>
     );
 };
