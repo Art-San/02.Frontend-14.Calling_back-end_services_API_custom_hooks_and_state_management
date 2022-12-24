31210e51fb51d1abf6762c8fa1b42ffb5b3dedd9
Aleksander <asava@sibmail.com>
2022-12-24 17:45:31 +0700

    Создание новых данных

--- src/app/pages/editQuality.jsx
+++ src/app/pages/editQuality.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import EditForm from "../components/ui/editForm";
+// get запрашиваем
+// post создаем объект
+// put изменяем существующий
+// delete -- соответственно
+
 
 const EditQualityPage = () => {
+    const [quality, setQuality] = useState(null)
+    const id = useParams().id
+    const qualityEndPoint = `http://localhost:4000/api/v1/quality/${id}`
+    
+    const handleSubmit = (data) => {
+        axios
+            .put(qualityEndPoint, data)
+            .then((res) => console.log('res.data.content', res.data.content))
+    }
+
+    useEffect(() => {
+        const loadData = async () => {
+            const { data } = await axios.get(qualityEndPoint)
+            setQuality(data.content)
+        }
+         loadData() 
+      }, []) 
     return (
         <>
-            <h1>Edit Quality Page</h1> <EditForm />
+            <h1>Edit Quality Page</h1>{' '}
+            {quality === null ? 'Loading...' : <EditForm data={quality} onSubmit={handleSubmit}/>}
         </>
     );
 };
