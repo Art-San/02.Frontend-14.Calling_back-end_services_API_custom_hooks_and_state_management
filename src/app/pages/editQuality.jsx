066157c726b4886840dfbfa3d6c8b2f274737368
Aleksander <asava@sibmail.com>
2022-12-25 00:17:51 +0700

    Глобальное отлавливание неожиданных ошибок

--- src/app/pages/editQuality.jsx
+++ src/app/pages/editQuality.jsx
@@ -2,32 +2,37 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import EditForm from "../components/ui/editForm";
+// response - ответ от сервера
+axios.interceptors.response.use(
+    (res) => res,
+    function(error) {
+        const expectedErrors = 
+            error.response &&
+            error.response.status >= 400 &&
+            error.response.status < 500
+
+        if (!expectedErrors) {
+            console.log('unexpected error')
+        }
+        return Promise.reject(error)
+})
 
 
 const EditQualityPage = () => {
     const [quality, setQuality] = useState(null)
     const id = useParams().id
     const qualityEndPoint = `http://localhost:4000/api/v1/quality/${id}`
+  
     
     const handleSubmit = async (data) => {
         try {
             await axios
-            .put(qualityEndPoint + 'fg', data)
+            .put(qualityEndPoint, data)
             .then((res) => console.log(res.data.content))
             
         } catch (error) {
-            const expectedErrors =
-                error.response &&
-                error.response.status >= 400 &&
-                error.response.status < 500
-            if (!expectedErrors) {
-                console.log('Упал сервер или чтото подобное')
-            } else {
-                console.log('Ожидаемая ошибка')
-            }
-            
+            console.log('Expected Error')
         }
-        
     }
 
     useEffect(() => {
