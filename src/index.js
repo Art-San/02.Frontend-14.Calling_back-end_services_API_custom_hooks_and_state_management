4e0cc846b7f6354c7e0889f2656e12af239b6e4a
Aleksander <asava@sibmail.com>
2022-12-25 20:46:46 +0700

    Логирование ошибок через https://sentry.io/

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'
-// import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import "./index.css";
 import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
+// https://sentry.io/
+Sentry.init({
+    dsn: "https://02ba5f4e8c0842a48e8cf81f8010e5d5@o4504388341989376.ingest.sentry.io/4504389412454400",
+    integrations: [new BrowserTracing()],
+  
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
@@ -16,14 +27,6 @@ root.render(
     </BrowserRouter>
 )
 
-// ReactDOM.render(
-//     <BrowserRouter>
-//         <React.StrictMode>
-//                 <App />
-//         </React.StrictMode>
-//     </BrowserRouter>,
-//     document.getElementById("root")
-// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
