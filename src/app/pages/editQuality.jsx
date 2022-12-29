519e1ccf1838a344b6176419e5b7831ec6b72571
Aleksander <asava@sibmail.com>
2022-12-30 00:09:28 +0700

    Редирект с Edit и add page

--- src/app/pages/editQuality.jsx
+++ src/app/pages/editQuality.jsx
@@ -1,16 +1,20 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import QualityForm from "../components/ui/qualityForm";
 import { useQualities } from "../hooks/useQualities";
 
 const EditQualityPage = () => {
+    const history = useHistory()
     const id = useParams().id
     
     const { getQuality, updateQuality } = useQualities()
     const quality = getQuality(id)
     const handleSubmit = (data) => {
-        console.log('data', data)
-        updateQuality(data)
+    
+        updateQuality(data).then(data => {
+            if (data) history.push('/')
+        })
     }
 
     return (
