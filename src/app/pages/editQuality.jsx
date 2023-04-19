8f28526af1485ff1fac3c690efa1fb96befc75b9
Aleksander <asava@sibmail.com>
2023-04-19 12:25:10 +0700

    Ожидаемые и неожиданные ошибки 19.04.23

--- src/app/pages/editQuality.jsx
+++ src/app/pages/editQuality.jsx
@@ -1,20 +1,20 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import EditForm from "../components/ui/editForm";
-
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import EditForm from '../components/ui/editForm'
 
 const EditQualityPage = () => {
     const [quality, setQuality] = useState(null)
     const id = useParams().id
     const qualityEndPoint = `http://localhost:4000/api/v1/quality/${id}`
-    
+
     const handleSubmit = async (data) => {
         try {
+            //Ожидаемые и неожиданные ошибки
             await axios
-            .put(qualityEndPoint + 'fg', data)
-            .then((res) => console.log(res.data.content))
-            
+                // .put(qualityEndPoint + 'fg', data) //+ 'fg' выдает Ожидаемую ошибку
+                .put(qualityEndPoint, data)
+                .then((res) => console.log(res.data.content))
         } catch (error) {
             const expectedErrors =
                 error.response &&
@@ -25,9 +25,7 @@ const EditQualityPage = () => {
             } else {
                 console.log('Ожидаемая ошибка')
             }
-            
         }
-        
     }
 
     useEffect(() => {
@@ -35,14 +33,18 @@ const EditQualityPage = () => {
             const { data } = await axios.get(qualityEndPoint)
             setQuality(data.content)
         }
-         loadData() 
-      }, []) 
+        loadData()
+    }, [])
     return (
         <>
             <h1>Edit Quality Page</h1>{' '}
-            {quality === null ? 'Loading...' : <EditForm data={quality} onSubmit={handleSubmit}/>}
+            {quality === null ? (
+                'Loading...'
+            ) : (
+                <EditForm data={quality} onSubmit={handleSubmit} />
+            )}
         </>
-    );
-};
+    )
+}
 
-export default EditQualityPage;
+export default EditQualityPage
