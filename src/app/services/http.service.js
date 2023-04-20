068f2f55014119ab31d8bd12f377ba9d09d5dfd6
Aleksander <asava@sibmail.com>
2023-04-20 20:37:56 +0700

    Логирование ошибок

--- src/app/services/http.service.js
+++ src/app/services/http.service.js
@@ -1,26 +1,26 @@
-import axios from "axios";
-import * as Sentry from '@sentry/react'
+import axios from 'axios'
+import * as Sentry from '@sentry/react' // Логирование ошибок
 // response - ответ от сервера
 axios.interceptors.response.use(
     (res) => res,
-    function(error) {
-        const expectedErrors = 
+    function (error) {
+        const expectedErrors =
             error.response &&
             error.response.status >= 400 &&
             error.response.status < 500
 
         if (!expectedErrors) {
-            Sentry.captureException(error)
+            Sentry.captureException(error) // Логирование ошибок
             console.log('unexpected error')
         }
         return Promise.reject(error)
-})
+    }
+)
 const httpService = {
     get: axios.get,
     post: axios.post,
     put: axios.put,
-    delete: axios.delete,
+    delete: axios.delete
 }
 
 export default httpService
-
--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
-import React from "react";
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import * as Sentry from "@sentry/react";
-import { BrowserTracing } from "@sentry/tracing";
-import "./index.css";
-import App from "./app/App";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.css";
+import * as Sentry from '@sentry/react' // Логирование ошибок
+// import { integrations } from '@sentry/tracing'
+import { BrowserTracing } from '@sentry/tracing'
+import './index.css'
+import App from './app/App'
+import reportWebVitals from './reportWebVitals'
+import { BrowserRouter } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.css'
 // https://sentry.io/
 Sentry.init({
-    dsn: "https://02ba5f4e8c0842a48e8cf81f8010e5d5@o4504388341989376.ingest.sentry.io/4504389412454400",
+    dsn: 'https://02ba5f4e8c0842a48e8cf81f8010e5d5@o4504388341989376.ingest.sentry.io/4504389412454400',
     integrations: [new BrowserTracing()],
-  
+
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
     // We recommend adjusting this value in production
-    tracesSampleRate: 1.0,
-  });
+    tracesSampleRate: 1.0
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
@@ -27,8 +28,7 @@ root.render(
     </BrowserRouter>
 )
 
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
