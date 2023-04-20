4e34fa3c93d4f01b84cf31708a1c29372454923b
Aleksander <asava@sibmail.com>
2023-04-20 19:07:16 +0700

    Глобальное отлавливание неожиданных ошибок

--- src/app/pages/editQuality.jsx
+++ src/app/pages/editQuality.jsx
@@ -1,12 +1,12 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import EditForm from "../components/ui/editForm";
-// response - ответ от сервера
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import EditForm from '../components/ui/editForm'
+// response - ответ от сервера // interceptors перехватчик запросов и ответов
 axios.interceptors.response.use(
     (res) => res,
-    function(error) {
-        const expectedErrors = 
+    function (error) {
+        const expectedErrors =
             error.response &&
             error.response.status >= 400 &&
             error.response.status < 500
@@ -15,21 +15,20 @@ axios.interceptors.response.use(
             console.log('Hепредвиденная ошибка')
         }
         return Promise.reject(error)
-})
-
+    }
+)
 
 const EditQualityPage = () => {
     const [quality, setQuality] = useState(null)
     const id = useParams().id
     const qualityEndPoint = `http://localhost:4000/api/v1/quality/${id}`
-  
-    
+
     const handleSubmit = async (data) => {
         try {
             await axios
-            .put(qualityEndPoint + 'hhh', data) // Ожидаемая ошибка + hhh
-            .then((res) => console.log(res.data.content))
-            
+                .put(qualityEndPoint, data)
+                // .put(qualityEndPoint + 'hhh', data) // Ожидаемая ошибка + hhh
+                .then((res) => console.log(res.data.content))
         } catch (error) {
             console.log('Ожидаемая ошибка')
         }
@@ -40,14 +39,18 @@ const EditQualityPage = () => {
             const { data } = await axios.get(qualityEndPoint)
             setQuality(data.content)
         }
-         loadData() 
-      }, []) 
+        loadData()
+    }, [])
     return (
         <>
             <h1>Edit Quality Page</h1>{' '}
-            {quality === null ? 'Loading...' : <EditForm data={quality} onSubmit={handleSubmit}/>}
+            {quality === null ? (
+                'Loading...'
+            ) : (
+                <EditForm data={quality} onSubmit={handleSubmit} />
+            )}
         </>
-    );
-};
+    )
+}
 
-export default EditQualityPage;
+export default EditQualityPage
