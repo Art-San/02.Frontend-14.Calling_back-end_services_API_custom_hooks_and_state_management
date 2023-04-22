844b611c337604be7e01dc3bb060546d819ea2a8
Aleksander <asava@sibmail.com>
2023-04-22 22:24:52 +0700

    Отображение ошибок с помощью toastify

--- src/app/App.js
+++ src/app/App.js
@@ -1,30 +1,32 @@
-import React from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
-import Container from "./components/common/container";
-import NavBar from "./components/ui/NavBar";
-import routes from "./routes";
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import React from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
+import Container from './components/common/container'
+import NavBar from './components/ui/NavBar'
+import routes from './routes'
+import { ToastContainer } from 'react-toastify' // Отображение ошибок с помощью toastify
+import 'react-toastify/dist/ReactToastify.css' // Отображение ошибок с помощью toastify
 
 const getRoutes = (routes) => {
     return routes.map((prop, key) => {
-        return <Route path={prop.path} component={prop.component} key={key} />;
-    });
-};
+        return <Route path={prop.path} component={prop.component} key={key} />
+    })
+}
 
 function App() {
     return (
-        <div className='App'>
+        <div className="App">
             <NavBar routes={routes} />
             <Container>
                 <Switch>
                     {getRoutes(routes)}
-                    <Redirect to='/' />
+                    <Redirect to="/" />
                 </Switch>
             </Container>
-            <ToastContainer/>
+            <ToastContainer
+            // Отображение ошибок с помощью toastify
+            />
         </div>
-    );
+    )
 }
 
-export default App;
+export default App
--- src/app/services/http.service.js
+++ src/app/services/http.service.js
@@ -1,27 +1,31 @@
-import axios from "axios";
+import axios from 'axios'
 import logger from './log.service'
-import { toast } from 'react-toastify'
+import { toast } from 'react-toastify' // Отображение ошибок с помощью toastify
+
+// В курсе используется react-toastify версии 8.1.0,
+// мы рекомендуем также устанавливать эту версию.
+// Установить её можно при помощи команды:
 // response - ответ от сервера
 axios.interceptors.response.use(
     (res) => res,
-    function(error) {
-        const expectedErrors = 
+    function (error) {
+        const expectedErrors =
             error.response &&
             error.response.status >= 400 &&
             error.response.status < 500
 
         if (!expectedErrors) {
             logger.log(error)
-            toast.error('Somthing was wrong. Try it leter')
+            toast.error('Somthing was wrong. Try it leter') // Отображение ошибок с помощью toastify
         }
         return Promise.reject(error)
-})
+    }
+)
 const httpService = {
     get: axios.get,
     post: axios.post,
     put: axios.put,
-    delete: axios.delete,
+    delete: axios.delete
 }
 
 export default httpService
-
